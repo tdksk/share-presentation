77c528ec1626fe1427f56cb96fa4af6f20f8f71b
Keisuke Tada <tdkskdt@gmail.com>
2012-09-10 18:09:05 +0900

    Fix degradation

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ io.configure(function () {
       // Get express.sid from cookie
       var sessionID = parseCookie(cookie)['connect.sid'];
       handshakeData.sessionID = sessionID;
-      
+
       // Get session from storage
       sessionStore.get(sessionID, function (err, session) {
         if (err) {
@@ -98,6 +98,30 @@ io.configure(function () {
   });
 });
 
+//  FIXME: Cannot work handshake.session.reload
+/*
+io.sockets.on('connection', function (socket) {
+  var _SESSION_UPDATE_INTERVAL = 3 * 1000;  //  3 seconds
+
+  var handshake = socket.handshake;
+  console.log('Connect:', handshake.sessionID);  //  For debug
+
+  var updateSessionTimerId = setInterval(function () {
+    //  Reload session
+    handshake.session.reload(function () {
+      //  Update lastAccess and maxAge
+      handshake.session.touch().save();
+      console.log('Update:', handshake.sessionID);  //  For debug
+    });
+  }, _SESSION_UPDATE_INTERVAL);
+
+  socket.on('disconnect', function () {
+    console.log('Disconnect:', handshake.sessionID);  //  For debug
+    clearInterval(updateSessionTimerId);
+  });
+});
+*/
+
 // TODO: Optimize
 var _userData = {},
     _pageCount = {
@@ -113,9 +137,7 @@ var presentation = io
   .of('/presentation')
   .on('connection', function (socket) {
     var sessionID = socket.id;
-    console.log('Connect:', sessionID);  // For debug
-    // io.of('/statistics').emit('statistics', _pageCount);
-    console.log(_pageCount);
+    console.log('Presentation connect:', sessionID);  // For debug
 
     socket.on('page', function (data) {
       socket.broadcast.emit('page', data);
@@ -150,6 +172,7 @@ var presentation = io
         case 'count':
           arr[data.pageNum]++;
           // io.of('/statistics').emit('statistics', _pageCount);
+          presentation.emit('user count', _pageCount);
           console.log(_pageCount);
           break;
         case 'discount':
@@ -203,7 +226,7 @@ var presentation = io
     });
 
     socket.on('disconnect', function () {
-      console.log('Disconnect:', sessionID);  // For debug
+      console.log('Presentation disconnect:', sessionID);  // For debug
       var data, arr;
       // If not after 'reset'
       if (_userData[sessionID]) {
@@ -216,6 +239,7 @@ var presentation = io
         delete _userData[sessionID];
       }
       // io.of('/statistics').emit('statistics', _pageCount);
+      presentation.emit('user count', _pageCount);
       console.log(_pageCount);
     });
 
