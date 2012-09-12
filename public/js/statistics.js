aa5aa94c8506986f3214460a9ca6713506a354d2
Keisuke Tada <tdkskdt@gmail.com>
2012-09-13 01:38:10 +0900

    Slight fix in statistics

--- public/js/statistics.js
+++ public/js/statistics.js
@@ -38,7 +38,7 @@
     _canvas.style.position = 'relative';
     _canvas.style.left = '0';
     _canvas.style.top = '0';
-    _canvas.style.zIndex = '1000';
+    _canvas.style.zIndex = '1';
     _canvas.style.float = 'left';
 
     // Before loaded
@@ -46,7 +46,10 @@
     ctx.fillText('Now Loading...', width / 2, height / 2);
     ctx.fill();
 
+    // Set graph
     _graph = new Graph(_canvas);
+    _graph.setType('bar');
+    _graph.setScale(15, 10);
   }
 
   /**
@@ -66,43 +69,46 @@
     _graph.clear();
 
     // Draw presenter page index
-    data = [];
-    arr = count.presenter;
-    presenterIndex = arr.indexOf(Math.max.apply(null, arr));
-    for (i = 0, length = arr.length; i < length; i++) {
-      data[i] = (arr[i]) ? [i + 1, 36] : [i + 1, 0];
+    if (count.presenter.length !== 0) {
+      data = [];
+      arr = count.presenter;
+      presenterIndex = arr.indexOf(Math.max.apply(null, arr));
+      for (i = 0, length = arr.length; i < length; i++) {
+        data[i] = (arr[i]) ? [i + 1, 36] : [i + 1, 0];
+      }
+      // Set data
+      _graph.setData(data);
+      // Set styles
+      _graph.setColor('rgba(224, 74, 40, .5)');
+      _graph.setBarWidth(2);
+      _graph.hideValue();
+      // Draw graph
+      _graph.draw();
     }
-    _graph.setData(data);
-    _graph.setColor('rgba(224, 74, 40, .5)');
-    _graph.setBarWidth(2);
-    _graph.hideValue();
-    _graph.draw();
 
     // Draw listener page view count
-    data = [];
-    arr = count.listener;
-
-    for (i = 0, length = arr.length; i < length; i++) {
-      // For graph
-      data[i] = [i + 1, arr[i]];
-      // For total count
-      total_user_count.listener += arr[i];
+    if (count.listener.length !== 0) {
+      data = [];
+      arr = count.listener;
+      for (i = 0, length = arr.length; i < length; i++) {
+        // For graph
+        data[i] = [i + 1, arr[i]];
+        // For total count
+        total_user_count.listener += arr[i];
+      }
+      // Set data
+      _graph.setData(data);
+      // Set styles
+      _graph.setColor('rgba(0, 122, 255, .8)');
+      _graph.setBarWidth(10);
+      _graph.showValue();
+      // Draw graph
+      _graph.draw();
     }
-
-    // Set data
-    _graph.setData(data);
-    // Set styles
-    _graph.setColor('rgba(0, 122, 255, .8)');
-    _graph.setType('bar');
-    _graph.setBarWidth(10);
-    _graph.setScale(15, 10);
-    _graph.showValue();
-    // Draw graph
-    _graph.draw();
 
     // Show total count
     var user_count = document.getElementById('user-count');
-    user_count.innerHTML = '<p>Listener: ' + total_user_count.listener + '</p>';
+    user_count.innerHTML = '<p>Total Listener: ' + total_user_count.listener + '</p>';
   });
 
   /**
