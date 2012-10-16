fc02eac18ef4173c177de2ace7e5f91ee2341e47
Keisuke Tada <tdkskdt@gmail.com>
2012-10-17 01:10:38 +0900

    Modify save function (using Ajax)

--- public/js/editor.js
+++ public/js/editor.js
@@ -8,7 +8,8 @@
       _params = getJsParam(),  // Load at first
       _user_id = _params.user_id,
       _presentation_id = _params.presentation_id,
-      _filePath;
+      _filePath,
+      _isSaved = true;
 
   var _CONTENTS_ID = 'editor',
       _CHANGE_ID = 'change',
@@ -17,7 +18,7 @@
 
   function confirm(event) {
     event = event || window.event;
-    return event.returnValue = '保存していない場合、変更が破棄されます';
+    if (!_isSaved) return event.returnValue = '保存していない場合、変更が破棄されます';
   }
 
   function initialize() {
@@ -51,7 +52,7 @@
 
     // Set action
     _editor.session.on('change', function () {
-      _onChange();
+      if (_isSaved) _onChange();
     });
   }
 
@@ -76,71 +77,90 @@
 
   function _onSaveFile() {
     var contents = _editor.session.getValue();
-    _execPost('/utils/write', {
+    _executeAjax('POST', '/utils/write', {
       user_id: _user_id
     , filePath: _filePath
     , contents: contents
+    }, function () {
+      _afterSaveFile();
     });
+  }
+
+  function _afterSaveFile() {
+    var change = document.getElementById(_CHANGE_ID);
+    change.disabled = true;
+    change.className = 'disabled';
+    change.innerText = 'Saved!';
+    _isSaved = true;
   }
 
   function _onChange() {
     var change = document.getElementById(_CHANGE_ID);
+    change.innerText = 'Save';
     change.disabled = false;
     change.className = null;
+    _isSaved = false;
+    console.log('onChange');
+  }
+
+  function _loadFile(filePath) {
+    _executeAjax('GET', filePath, null, function () {
+      var contents = document.createTextNode(_xmlHttp.responseText);
+      document.getElementById(_CONTENTS_ID).appendChild(contents);
+    });
   }
 
   /**
-   * Load file
+   * Utilities
    */
-  function _loadFile(filePath) {
+  function _executeAjax(type, url, data, callback) {
+    var async = true,
+        sendContent = null;
+
     if (window.XMLHttpRequest) {
       _xmlHttp = new XMLHttpRequest();
     } else {
       _xmlHttp = null;
     }
 
-    _xmlHttp.onreadystatechange = _checkStatus;
-    _xmlHttp.open('GET', filePath, false);
-    _xmlHttp.send(null);
-  }
+    _xmlHttp.onreadystatechange = function () {
+      var READYSTATE_COMPLETED = 4,
+          HTTP_STATUS_OK = 200;
 
-  function _checkStatus() {
-    if (_xmlHttp.readyState == 4 && _xmlHttp.status == 200) {
-      var contents = document.createTextNode(_xmlHttp.responseText);
-      document.getElementById(_CONTENTS_ID).appendChild(contents);
-    }
-  }
-
-  /**
-   * Utilities
-   */
-  function _execPost(action, data) {
-    // Create form
-    var form = document.createElement('form');
-    form.setAttribute('action', action);
-    form.setAttribute('method', 'post');
-    form.style.display = 'none';
-    document.body.appendChild(form);
-    // Configure parameters
-    if (data !== undefined) {
-      for (var paramName in data) {
-        var input = document.createElement('input');
-        input.setAttribute('type', 'hidden');
-        input.setAttribute('name', paramName);
-        input.setAttribute('value', data[paramName]);
-        form.appendChild(input);
+      if (_xmlHttp.readyState == READYSTATE_COMPLETED
+          && _xmlHttp.status == HTTP_STATUS_OK) {
+        if (callback) callback();
       }
+    };
+
+    if (type === 'GET') async = false;
+    _xmlHttp.open(type, url, async);
+
+    if (type === 'POST') {
+      _xmlHttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=utf-8');
+      sendContent = _encodeHTMLForm(data);
     }
-    // submit
-    form.submit();
+
+    _xmlHttp.send(sendContent);
+  }
+
+  function _encodeHTMLForm(data) {
+    var params = [],
+        name;
+    for (name in data) {
+      var value = data[name];
+      var param = encodeURIComponent( name ).replace(/%20/g, '+')
+            + '=' + encodeURIComponent( value ).replace(/%20/g, '+');
+      params.push(param);
+    }
+    return params.join('&');
   }
 
   /**
    * Initialize
    */
   window.addEventListener('DOMContentLoaded', initialize, false);
-  // TODO: Disable confirm when saving
-  // window.addEventListener('beforeunload', confirm, false);
+  window.addEventListener('beforeunload', confirm, false);
 
   /**
    * Export
