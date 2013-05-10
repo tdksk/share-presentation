c73da250f2fe8d3a7c9093f6d88258c874abaf24
Keisuke Tada <tdkskdt@gmail.com>
2013-05-10 18:20:01 +0900

    Export ajax function

--- public/js/editor.js
+++ public/js/editor.js
@@ -80,7 +80,7 @@
     _changeElem.innerText = 'Saving...';
     // Save data
     var data = _editor.session.getValue();
-    _ajax({
+    ajax({
       type: 'POST',
       url: '/presentation/update',
       data: {
@@ -105,7 +105,7 @@
 
     // Show preview
     var previewUrl = '/p/' + _user_id + '/' + _presentation_id + '/';
-    _ajax({
+    ajax({
       type: 'GET',
       url: previewUrl,
       success: function (data) {
@@ -114,66 +114,6 @@
         document.getElementById('preview').innerHTML = contents;
       }
     });
-  }
-
-  /**
-   * Utilities
-   */
-  function _ajax(options) {
-    options = options || {};
-
-    var async = (options.type === 'GET') ? false : true,
-        sendContent = null;
-
-    _xmlHttp = _createHttpRequest();
-
-    _xmlHttp.onreadystatechange = function () {
-      var READYSTATE_COMPLETED = 4,
-          HTTP_STATUS_OK = 200;
-      if (_xmlHttp.readyState == READYSTATE_COMPLETED) {
-        if (_xmlHttp.status == HTTP_STATUS_OK) {
-          if (options.success) options.success(_xmlHttp);
-        } else {
-          if (options.error) options.error(_xmlHttp);
-        }
-      }
-    };
-
-    _xmlHttp.open(options.type, options.url, async);
-
-    if (options.type === 'POST') {
-      _xmlHttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=utf-8');
-      sendContent = _encodeHTMLForm(options.data);
-    }
-
-    _xmlHttp.send(sendContent);
-  }
-
-  function _createHttpRequest() {
-    var x = null;
-    if (window.XMLHttpRequest) return new XMLHttpRequest();
-    try {
-      return new ActiveXObject("Msxml2.XMLHTTP");
-    } catch (e) {
-      try {
-        return new ActiveXObject("Microsoft.XMLHTTP");
-      } catch (e) {
-        x = null;
-      }
-    }
-    return x;
-  }
-
-  function _encodeHTMLForm(data) {
-    var params = [],
-        name;
-    for (name in data) {
-      var value = data[name];
-      var param = encodeURIComponent( name ).replace(/%20/g, '+')
-            + '=' + encodeURIComponent( value ).replace(/%20/g, '+');
-      params.push(param);
-    }
-    return params.join('&');
   }
 
   /**
--- public/js/utils.js
+++ public/js/utils.js
@@ -31,3 +31,61 @@ var loadCss = function (href) {
   css.setAttribute('href', href);
   head.appendChild(css);
 };
+
+/* Ajax */
+var ajax = function (options) {
+  options = options || {};
+
+  var async = (options.type === 'GET') ? false : true,
+      sendContent = null;
+
+  var _xmlHttp = _createHttpRequest();
+
+  _xmlHttp.onreadystatechange = function () {
+    var READYSTATE_COMPLETED = 4,
+        HTTP_STATUS_OK = 200;
+    if (_xmlHttp.readyState == READYSTATE_COMPLETED) {
+      if (_xmlHttp.status == HTTP_STATUS_OK) {
+        if (options.success) options.success(_xmlHttp);
+      } else {
+        if (options.error) options.error(_xmlHttp);
+      }
+    }
+  };
+
+  _xmlHttp.open(options.type, options.url, async);
+
+  if (options.type === 'POST') {
+    _xmlHttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=utf-8');
+    sendContent = _encodeHTMLForm(options.data);
+  }
+
+  _xmlHttp.send(sendContent);
+
+  function _createHttpRequest() {
+    var x = null;
+    if (window.XMLHttpRequest) return new XMLHttpRequest();
+    try {
+      return new ActiveXObject("Msxml2.XMLHTTP");
+    } catch (e) {
+      try {
+        return new ActiveXObject("Microsoft.XMLHTTP");
+      } catch (e) {
+        x = null;
+      }
+    }
+    return x;
+  }
+
+  function _encodeHTMLForm(data) {
+    var params = [],
+        name;
+    for (name in data) {
+      var value = data[name];
+      var param = encodeURIComponent( name ).replace(/%20/g, '+')
+            + '=' + encodeURIComponent( value ).replace(/%20/g, '+');
+      params.push(param);
+    }
+    return params.join('&');
+  }
+};
